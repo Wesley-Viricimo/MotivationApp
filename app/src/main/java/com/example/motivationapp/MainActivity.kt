91d9cd4e843d628872ef0863d426fd4d56e39fe5
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2023-10-21 23:08:21 -0300

    Alterando cor primária e iniciando view binding

--- app/src/main/java/com/example/motivationapp/MainActivity.kt
+++ app/src/main/java/com/example/motivationapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.motivationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.motivationapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener{
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) //layoutInflater vem do AppCompatActivity que nossa classe herda
+        setContentView(binding.root) //Atribuindo o binding para a view
+
+        supportActionBar?.hide() //Se a supportActionBar existir, será escondida
+
+        binding.btnNovaFrase.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if(view.id == R.id.btn_nova_frase) {
+
+        }
     }
 }
